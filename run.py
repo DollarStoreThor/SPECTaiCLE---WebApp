ac683be23eb7560c3e61a7c90643c60fe0ceb4ae
Andrew L Thomas <68823420+DollarStoreThor@users.noreply.github.com>
2025-01-02 13:05:56 -0600

    Add files via upload

--- run.py
+++ run.py
@@ -4,18 +4,5 @@ import pandas as pd
 
 app = create_app()
 
-@app.route('/')
-def index():
-    # Read CSV file
-    df = pd.read_csv(r"C:\Users\arado\Desktop\SPECTaiCLE\model\recommendation_csv_files\b.csv")
-    
-    # Extract unique values for dropdown
-    dropdown_options = df['AuthorTitleString'].unique().tolist()
-    
-    # Sort options alphabetically (optional)
-    dropdown_options.sort()
-    
-    return render_template('index.html', dropdown_options=dropdown_options)
-
 if __name__ == "__main__":
     app.run(debug=True)
